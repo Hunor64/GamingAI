e0b82a74f6d952f5de765c7a21f83c242464442c
hunor64 <nhunor06@gmail.com>
2024-05-14 12:47:13 +0200

    Good

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function CreatePlayers(p) {
         }
     }
     setTimeout(console.log("Started Interval"), 1000)
-    inter = setInterval(MovePlayers, 1000)
+    //inter = setInterval(MovePlayers, 1000)
 
     document.querySelector(".btnIndit").disabled = true;
 }
@@ -124,92 +124,44 @@ function MovePlayers() {
 
             }
         }
-
-
         selectedElement.innerHTML = ""
+
+
         let moveDir = Math.floor(Math.random() * (4 - 1)) + 1
-        let moved = false
-        let whileCount = 0
-        while (!moved || whileCount != 3) {
-            whileCount++
-            if (moveDir == 1) {
-                if (element.row > 0 && !IsPlayerThere(element.row - 1, element.column)) {
-                    element.row--
-                    moved = true
-                    break
-                }
-                else {
-                    moveDir = 2
-                    console.log("moved to 2")
-                }
-            }
-            if (moveDir == 2) {
-                if (element.row < rows - 1 && !IsPlayerThere(element.row + 1, element.column)) {
-                    element.row++
-                    moved = true
-                    break
-                }
-                else {
-                    moveDir = 3
-                    console.log("moved to 3")
-                }
-            }
-            if (moveDir == 3) {
-                if (element.column > 0 && !IsPlayerThere(element.row, element.column - 1)) {
-                    element.column--
-                    moved = true
-                    break
-                }
-                else {
-                    moveDir = 4
-                    console.log("moved to 4")
+        let moves = GetAllPossibleMoves(element.row,element.column)
+        console.log("Possible moves",moves)
 
-                }
-            }
-            if (moveDir == 4) {
-                if (element.column < columns - 1 && !IsPlayerThere(element.row, element.column + 1)) {
-                    element.column++
-                    moved = true
-                    break
-                }
-                else {
-                    moveDir = 1
-                    console.log("moved to 1")
-                }
-            }
-        }
-        if (moved) {
-            element.class = "c" + element.column + "r" + element.row
-            mines.forEach(selectedMine => {
-                if (element.row == selectedMine.row && element.column == selectedMine.column) {
-                    kill(element,"deadly red goo")
-                }
-            })
-        }
-        else {
-            kill(element,"other players")
-        }
+        
 
+        if (moves.length == 0) {
+            kill(element)
+        }
         PutAllPlayersOn()
 
     })
 }
-function IsPlayerThere(row, column) {
-    let i = players.findIndex(e => e.row === row && e.column === column);
-    let mines = blackList.findIndex(e => e.row === row && e.column === column);
-    if (i !== -1 && mines !== -1) {
-        console.log(e.row , row)
-        return true
-    }
-    else {
-        return false
-    }
+function GetAllPossibleMoves(row,column){
+    let moves = [[1,0],[-1,0],[0,1],[0,-1]]
+    let canMove = []
+    moves.forEach(move =>{
+        let isPlayerThere = players.findIndex(e => e.row === row-move[0] && e.column === column-move[1])
+        let isMineThere = blackList.findIndex(e => e.row === row-move[0] && e.column === column-move[1])
+        let wallNotThere = true
+        if (row-move[0]>rows-1||row-move[0]<0||column-move[1]>columns-1||column-move[1]<0) {
+            wallNotThere = false
+        }
+        if (isPlayerThere == -1 && isMineThere == -1 &&wallNotThere) {
+            canMove.push(move)
+        }
+        console.log(isPlayerThere)
+    })
+    return canMove
 }
 
 function kill(element,deathMSG) {
     let li = document.createElement("li")
-    li.innerHTML = "Player " + element.id + " was kiled by "+deathMSG
-    console.log("Player " + element.id + " was kiled by "+deathMSG)
+    li.innerHTML = "Player " + element.id + " was kiled"
+    console.log("Player " + element.id + " was kiled")
     KILLS.appendChild(li)
     let index = players.findIndex(e => e.id === element.id);
     blackList.push({
@@ -217,7 +169,7 @@ function kill(element,deathMSG) {
         "column": element.column
     })
     if (index !== -1) {
-        players.splice(index, 1);
+        players.splice(index, 1)
     }
     console.log(blackList)
 }
