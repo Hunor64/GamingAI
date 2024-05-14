0dc5471624eac030f7a0e60ce2e121869b381498
hunor64 <nhunor06@gmail.com>
2024-05-14 13:22:54 +0200

    Better Code

--- script.js
+++ script.js
@@ -7,14 +7,6 @@ let players = []
 let mines = []
 let blackList = []
 
-function ClearDiv() {
-    while (GAMEDIV.lastChild) {
-        GAMEDIV.removeChild(GAMEDIV.lastChild)
-    }
-}
-
-
-
 let rows = 0
 let columns = 0
 
@@ -22,7 +14,9 @@ let columns = 0
 function felvesz(m, n) {
     clearInterval(inter)
     avalibleLocations = []
-    ClearDiv()
+    while (GAMEDIV.lastChild) {
+        GAMEDIV.removeChild(GAMEDIV.lastChild)
+    }
     rows = n
     columns = m
     for (let i = 0; i < m; i++) {
@@ -37,13 +31,14 @@ function felvesz(m, n) {
         GAMEDIV.appendChild(row)
     }
     document.querySelector(".btnIndit").disabled = false;
+
     players = []
     mines = []
-    document.querySelector(".kills").innerHTML = ""
+    KILLS = ""
 }
 
 function UpdateSliders() {
-    let max = Math.floor(document.querySelector(".height").value*document.querySelector(".width").value/2)
+    let max = Math.floor(document.querySelector(".height").value * document.querySelector(".width").value / 2)
     document.querySelector(".mine").value = Math.min(document.querySelector(".mine").value, max)
     document.querySelector(".players").value = Math.min(document.querySelector(".players").value, max)
     document.querySelector(".mine").max = max
@@ -52,33 +47,25 @@ function UpdateSliders() {
 
 
 function CreatePlayers(p) {
-    felvesz(document.querySelector('.height').value,document.querySelector('.width').value)
+    felvesz(document.querySelector('.height').value, document.querySelector('.width').value)
     SpawnMines(document.querySelector(".mine").value)
 
     for (let i = 0; i < p; i++) {
-        let isDed = false
         let location = RandomGen()
-        let randomRow = location[0] 
+        let randomRow = location[0]
         let randowColumn = location[1]
 
-        if (!isDed) {
-            players.push({
-                "id": i + 1,
-                "class": "c" + randowColumn + "r" + randomRow,
-                "row": randomRow,
-                "column": randowColumn
-            })
-            let selectedElement = document.querySelector(".c" + randowColumn + "r" + randomRow)
-            let playerChar = document.createElement("div")
-            playerChar.innerText = i + 1
-            playerChar.classList.add("player")
-            selectedElement.appendChild(playerChar)
-        }
-        else {
-            let li = document.createElement("li")
-            li.innerHTML = "Player " + i + " was born dead"
-            KILLS.appendChild(li)
-        }
+        players.push({
+            "id": i + 1,
+            "class": "c" + randowColumn + "r" + randomRow,
+            "column": randowColumn,
+            "row": randomRow
+        })
+        let selectedElement = document.querySelector(".c" + randowColumn + "r" + randomRow)
+        let playerChar = document.createElement("div")
+        playerChar.innerText = i + 1
+        playerChar.classList.add("player")
+        selectedElement.appendChild(playerChar)
     }
     setTimeout(console.log("Started Interval"), 1000)
     inter = setInterval(MovePlayers, 1000)
@@ -88,15 +75,14 @@ function CreatePlayers(p) {
 
 function SpawnMines(n) {
     for (let i = 0; i < n; i++) {
-        let location = RandomGen(0, rows, true, false)
-        let randomRow = location[0] 
+        let location = RandomGen()
+        let randomRow = location[0]
         let randowColumn = location[1]
         mines.push({
             "row": randomRow,
             "column": randowColumn
         })
-        let selectedElement = document.querySelector(".c" + randowColumn + "r" + randomRow)
-        selectedElement.classList.add("mine")
+        document.querySelector(".c" + randowColumn + "r" + randomRow).classList.add("mine")
     }
 }
 function PutAllPlayersOn() {
@@ -122,35 +108,35 @@ function MovePlayers() {
         }
 
 
-        let moves = GetAllPossibleMoves(element.row,element.column)
+        let moves = GetAllPossibleMoves(element.row, element.column)
         if (moves.length == 0) {
             kill(element)
         }
-        else{
+        else {
             let moveInd = Math.floor(Math.random() * moves.length)
             let whereToMove = moves[moveInd]
-            element.row= element.row - whereToMove[0]
-            
-            element.column= element.column - whereToMove[1]
+            element.row = element.row - whereToMove[0]
+
+            element.column = element.column - whereToMove[1]
             element.class = "c" + element.column + "r" + element.row
         }
         let isOnMine = mines.findIndex(e => e.row === element.row && e.column === element.column)
         if (isOnMine != -1) {
             kill(element)
         }
-        
+
         PutAllPlayersOn()
 
     })
 }
-function GetAllPossibleMoves(row,column){
-    let moves = [[1,0],[-1,0],[0,1],[0,-1]]
+function GetAllPossibleMoves(row, column) {
+    let moves = [[1, 0], [-1, 0], [0, 1], [0, -1]]
     let canMove = []
-    moves.forEach(move =>{
-        let isPlayerThere = players.findIndex(e => e.row === row-move[0] && e.column === column-move[1])
-        let isMineThere = blackList.findIndex(e => e.row === row-move[0] && e.column === column-move[1])
+    moves.forEach(move => {
+        let isPlayerThere = players.findIndex(e => e.row === row - move[0] && e.column === column - move[1])
+        let isMineThere = blackList.findIndex(e => e.row === row - move[0] && e.column === column - move[1])
         let wallNotThere = true
-        if (row-move[0]>rows-1||row-move[0]<0||column-move[1]>columns-1||column-move[1]<0) {
+        if (row - move[0] > rows - 1 || row - move[0] < 0 || column - move[1] > columns - 1 || column - move[1] < 0) {
             wallNotThere = false
         }
         if (isPlayerThere == -1 && isMineThere == -1 && wallNotThere) {
@@ -177,15 +163,15 @@ function kill(element) {
 }
 
 function RandomGen() {
-    if(avalibleLocations.length == 0){
+    if (avalibleLocations.length == 0) {
         for (let generatedRows = 0; generatedRows < rows; generatedRows++) {
             for (let generatedColumns = 0; generatedColumns < columns; generatedColumns++) {
-                avalibleLocations.push([generatedRows,generatedColumns])
-            }                        
+                avalibleLocations.push([generatedRows, generatedColumns])
+            }
         }
     }
-    let generatedNumber = Math.floor(Math.random() * (avalibleLocations.length) )
+    let generatedNumber = Math.floor(Math.random() * (avalibleLocations.length))
     let genCoords = avalibleLocations[generatedNumber]
-    avalibleLocations.splice(generatedNumber,1)
+    avalibleLocations.splice(generatedNumber, 1)
     return genCoords
 }
